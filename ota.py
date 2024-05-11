65d12a03efe34eabb1b1b52507ed813fa554604a
andreasp <andreas@pedersenmail.com>
2024-05-11 15:51:55 +0200

    add initial ota.py

--- ota.py
+++ ota.py
@@ -2,6 +2,15 @@ from transistor import MosDevice()
 
 class OTA:
     def __init__(self):
-        M1 = MosDevice()
-        M3 = MosDevice()
-        M7 = MosDevice()
+        self.M1 = MosDevice()
+        self.M3 = MosDevice()
+        self.M7 = MosDevice()
+        
+        self.id: float = 0.0
+        
+    def init(self) -> None:
+        if self.M1.valid() and self.M3.valid() and self.M7.valid():
+            self.__calculate()
+            
+    def __calculate(self) -> None:
+        pass
