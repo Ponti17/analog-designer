7211070d8a5c176646b91c6827487869114ac25d
andreasp <andreas@pedersenmail.com>
2024-05-14 19:18:58 +0200

    add CL and GM attributes and calculate dom pole

--- folded_cascode.py
+++ folded_cascode.py
@@ -1,4 +1,5 @@
 from transistor import MosDevice
+import numpy as np
 
 # Full input-swing folded cascode
 # P. 390 Razavi
@@ -19,9 +20,10 @@ class FoldedCascode:
         self.M5 = MosDevice()
         
         self.itail: float   = 0.0
-        self.GM: float      = 0.0
+        self.GM_val: float      = 0.0
         self.av_val: float      = 0.0
         self.rout_val: float    = 0.0
+        self.CL: float      = 0.0
         
         # Output Pole
         self.fp1: float     = 0.0
@@ -50,14 +52,22 @@ class FoldedCascode:
             
     def __calculate(self) -> None:
         # Assume worst case GM
-        self.GM = min(self.M0.gm(), self.M1.gm())
+        self.GM_val = min(self.M0.gm(), self.M1.gm())
         nmos_ro = self.__cascode(self.M2.ro(), self.M3.ro(), self.M3.gm())
         pmos_ro = self.__cascode(self.M4.ro(), self.M5.ro(), self.M5.gm())
         self.rout_val = self.__parallel(nmos_ro, pmos_ro)
-        self.av_val = self.rout_val * self.GM
+        self.av_val = self.rout_val * self.GM_val
+        
+        self.fp1 = 1 / (2 * np.pi * self.rout_val * self.CL)
         
     def av(self) -> float:
         return self.av_val
     
     def rout(self) -> float:
         return self.rout_val
+    
+    def poles(self) -> list[float]:
+        return [self.fp1]
+
+    def GM(self) -> float:
+        return self.GM_val
