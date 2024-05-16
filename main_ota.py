b9add9585a4e63b15003b82d751600c7bd8c312b
andreasp <andreas@pedersenmail.com>
2024-05-16 20:44:05 +0200

    update OTA with new circuit values

--- main_ota.py
+++ main_ota.py
@@ -3,20 +3,20 @@ from ota import OTA
 
 if __name__ == "__main__":
     ota = OTA()
-    ota.itail = 2.2e-6
+    ota.itail = 2.24e-6 * 2
     ota.CL = 400e-15
     
     ota.M0.model = "pch_25"
-    ota.M1.model = "nch_25"
+    ota.M1.model = "nch"
     ota.M2.model = "nch"
-    ota.M3.model = "pch_25"
-    ota.M4.model = "pch"
+    ota.M3.model = "pch"
+    ota.M4.model = "pch_hvt"
     
     ota.M0.gateL = 1e-6
-    ota.M1.gateL = 20e-6
-    ota.M2.gateL = 1e-6
-    ota.M3.gateL = 10e-6
-    ota.M4.gateL = 1e-6
+    ota.M1.gateL = 2e-6
+    ota.M2.gateL = 17e-6
+    ota.M3.gateL = 2e-6
+    ota.M4.gateL = 2e-6
 
     ota.M0.vdsrc = 0.3
     ota.M1.vdsrc = 0.3
@@ -25,10 +25,10 @@ if __name__ == "__main__":
     ota.M4.vdsrc = 0.3
     
     ota.M0.gmoverid = 27
-    ota.M1.gmoverid = 15
-    ota.M2.gmoverid = 9
-    ota.M3.gmoverid = 12
-    ota.M4.gmoverid = 7
+    ota.M1.gmoverid = 22
+    ota.M2.gmoverid = 16
+    ota.M3.gmoverid = 20
+    ota.M4.gmoverid = 18
     
     ota.init()
     
@@ -38,3 +38,6 @@ if __name__ == "__main__":
     
     poles = ota.poles()
     print(poles)
+    
+    sizes = ota.size()
+    print(sizes)
