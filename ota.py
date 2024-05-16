1fd192ba2f41fdd791aeb18435454cbcdd93250f
andreasp <andreas@pedersenmail.com>
2024-05-16 13:52:08 +0200

    update ota.py

--- ota.py
+++ ota.py
@@ -1,14 +1,34 @@
+import numpy as np
+from utils import Utils
 from transistor import MosDevice
+
+# Three-mirror OTA
+# "A Low-Power, Low-Noise CMOS Amplifier for Neural Recording Applications"
+# Reid R. Harrison
 
 class OTA:
     def __init__(self) -> None:
-        self.M1 = MosDevice()
-        self.M3 = MosDevice()
-        self.M7 = MosDevice()
+        self.utils = Utils()
         
-        self.itail: float   = 0.0
-        self.av_val: float      = 0.0
+        # Input PMOS
+        self.M0 = MosDevice()
+        
+        # NMOS Mirror
+        self.M1 = MosDevice()
+        
+        # NMOS Cascode
+        self.M2 = MosDevice()
+        
+        # PMOS Mirror
+        self.M3 = MosDevice()
+        
+        # PMOS Cascode
+        self.M4 = MosDevice()
+        
+        self.itail: float       = 0.0
+        self.GM_val: float      = 0.0
         self.rout_val: float    = 0.0
+        self.CL: float          = 0.0
         
         # Output Pole
         self.fp1: float     = 0.0
@@ -20,21 +40,35 @@ class OTA:
         self.fp4: float     = 0.0
         
     def init(self) -> None:
+        self.M0.set_id(self.itail/2)
         self.M1.set_id(self.itail/2)
+        self.M2.set_id(self.itail/2)
         self.M3.set_id(self.itail/2)
-        self.M7.set_id(self.itail/2)
-        if self.M1.valid() and self.M3.valid() and self.M7.valid():
-            self.__calculate()
-            
-    def __parallel(self, R1: float, R2: float) -> float:
-        return R1 * R2 / (R1 + R2)
+        self.M4.set_id(self.itail/2)
+        
+        self.__calculate()
             
     def __calculate(self) -> None:
-        self.rout_val = self.__parallel(self.M3.ro(), self.M7.ro())
-        self.av_val = self.rout_val * self.M1.gm()
+        self.GM_val = self.M0.gm()
+        nmos_ro = self.utils.cascode(self.M1.ro(), self.M2.ro(), self.M2.gm())
+        pmos_ro = self.utils.cascode(self.M3.ro(), self.M4.ro(), self.M4.gm())
+        self.rout_val = self.utils.parallel([nmos_ro, pmos_ro])
+        
+        self.fp1 = 1 / (2 * np.pi * self.rout_val * self.CL)
+        self.fp2 = self.M1.gm() / (2 * np.pi * self.M1.cgg())
+        self.fp3 = self.M1.gm() / (2 * np.pi * self.M1.cgg())
+        self.fp4 = self.M3.gm() / (2 * np.pi * self.M3.cgg())
+        
+        self.av_val = self.rout_val * self.M0.gm()
         
     def av(self) -> float:
-        return self.av_val
+        return self.rout_val * self.GM_val
     
     def rout(self) -> float:
         return self.rout_val
+    
+    def poles(self) -> list[float]:
+        return [self.fp1, self.fp2, self.fp3, self.fp4]
+    
+    def GM(self) -> float:
+        return self.GM_val
