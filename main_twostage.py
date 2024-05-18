b70ef870fcadae842d3d78276bbf67074e322759
andreasp <andreas@pedersenmail.com>
2024-05-18 11:16:03 +0200

    add initial two stage model

---
+++ main_twostage.py
@@ -0,0 +1,47 @@
+import numpy as np
+from twostage import TwoStage
+
+if __name__ == "__main__":
+    ts = TwoStage()
+    ts.itail    = 2.8e-6
+    ts.iout     = 2.8e-6
+    ts.CL       = 400e-15
+    ts.cl_gain  = 20*np.log10(20)
+    
+    ts.M0.model = "pch_25"
+    ts.M1.model = "nch_25"
+    ts.M2.model = "nch_25"
+    ts.M3.model = "pch_25"
+    ts.M4.model = "pch_25"
+    
+    ts.M0.gateL = 1e-6
+    ts.M1.gateL = 8e-6
+    ts.M2.gateL = 3e-6
+    ts.M3.gateL = 10e-6
+    ts.M4.gateL = 1e-6
+
+    ts.M0.vdsrc = 0.3
+    ts.M1.vdsrc = 0.3
+    ts.M2.vdsrc = 0.3
+    ts.M3.vdsrc = 0.3
+    ts.M4.vdsrc = 0.3
+    
+    ts.M0.gmoverid = 27
+    ts.M1.gmoverid = 22
+    ts.M2.gmoverid = 15
+    ts.M3.gmoverid = 17
+    ts.M4.gmoverid = 15
+    
+    ts.init()
+    
+    av = ts.av()
+    av_db = 20 * np.log10(av)
+    print(av_db)
+    
+    poles = ts.poles()
+    print(poles)
+    
+    sizes = ts.size()
+    print(sizes)
+    
+    # ts.bode()
--- twostage.py
+++ twostage.py
@@ -1,17 +1,121 @@
 import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from utils import Utils
 from transistor import MosDevice
 
-# Two-stage amplifier structure w/ PMOS inputs
+# Three-mirror OTA
+# "A Low-Power, Low-Noise CMOS Amplifier for Neural Recording Applications"
+# Reid R. Harrison
 
-class TwoStage():
+class OTA:
     def __init__(self) -> None:
+        self.utils = Utils()
+        
+        # Closed loop gain
+        self.cl_gain: float = 0.0
+        
+        # Input PMOS
+        self.M0 = MosDevice()
+        
+        # NMOS Mirror
+        self.M1 = MosDevice()
+        
+        # NMOS Cascode
         self.M2 = MosDevice()
+        
+        # PMOS Mirror
+        self.M3 = MosDevice()
+        
+        # PMOS Cascode
         self.M4 = MosDevice()
-        self.M6 = MosDevice()
-        self.M7 = MosDevice()
         
-        self.itail: float  = 0.0
-        self.vdd: float    = 0.0
-        self.CL: float     = 0.0
+        self.iout: float        = 0.0
+        self.itail: float       = 0.0
+        self.GM_val: float      = 0.0
+        self.rout_val: float    = 0.0
+        self.CL: float          = 0.0
+        
+        # Output Pole
+        self.fp1: float     = 0.0
+        # NMOS Mirror Pole
+        self.fp2: float     = 0.0
+        # NMOS Mirror Pole
+        self.fp3: float     = 0.0
+        # PMOS Mirror Pole
+        self.fp4: float     = 0.0
+        
+    def init(self) -> None:
+        self.M0.set_id(self.itail/2)
+        self.M1.set_id(self.itail/2)
+        self.M2.set_id(self.itail/2)
+        self.M3.set_id(self.itail/2)
+        self.M4.set_id(self.itail/2)
+        
+        self.__calculate()
+            
+    def __calculate(self) -> None:
+        self.GM_val = self.M0.gm()
+        nmos_ro = self.utils.cascode(self.M1.ro(), self.M2.ro(), self.M2.gm())
+        pmos_ro = self.utils.cascode(self.M3.ro(), self.M4.ro(), self.M4.gm())
+        self.rout_val = self.utils.parallel([nmos_ro, pmos_ro])
+        
+        self.fp1 = (1 / (2 * np.pi * self.rout_val * self.CL)).item()
+        self.fp2 = (self.M1.gm() / (4 * np.pi * self.M1.cgs())).item()
+        self.fp3 = (self.M1.gm() / (4 * np.pi * self.M1.cgs())).item()
+        self.fp4 = (self.M3.gm() / (4 * np.pi * self.M3.cgs())).item()
+        
+        self.av_val = self.rout_val * self.M0.gm()
+        
+    def av(self) -> float:
+        return self.rout_val * self.GM_val
+    
+    def rout(self) -> float:
+        return self.rout_val
+    
+    def poles(self) -> list[float]:
+        return [self.fp1, self.fp2, self.fp3, self.fp4]
+    
+    def GM(self) -> float:
+        return self.GM_val
+    
+    def size(self) -> dict[str, str]:
+        W0 = str("{:.2e}".format(float(self.M0.w_val)))
+        W1 = str("{:.2e}".format(float(self.M1.w_val)))
+        W2 = str("{:.2e}".format(float(self.M2.w_val)))
+        W3 = str("{:.2e}".format(float(self.M3.w_val)))
+        W4 = str("{:.2e}".format(float(self.M4.w_val)))
+        return {"W0": W0, "W1": W1, "W2": W2, "W3": W3, "W4": W4}
+    
+    def bode(self) -> None:
+        poles = self.poles()
+        p1 = poles[0]
+        p2 = poles[1]
+        p3 = poles[2]
+        p4 = poles[3]
+        den4 = [1, p1+p2+p3+p4, p1*p2+p1*p3+p1*p4+p2*p3+p2*p4+p3*p4, p1*p2*p3+p1*p2*p4+p1*p3*p4+p2*p3*p4, p1*p2*p3*p4]
+        K = den4[-1] * self.av()
+        
+        sys = sp.signal.TransferFunction([K], den4)
+        w, mag, phase = sp.signal.bode(sys)
 
-        
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 8)
+
+        ax.set_xlabel('Frequency [Hz]')
+        ax.set_ylabel('Magnitude [dB]')
+        ax.semilogx(w, mag)
+
+        ax2 = ax.twinx()
+        ax2.semilogx(w, phase, color="red")
+        ax2.set_ylabel('Phase [deg]')
+        ax.grid()
+
+        for i in range(0, len(mag)):
+            if mag[i] < self.cl_gain:
+                print("Phase Margin: {}".format(180+phase[i]))
+                break
+
+        plt.title("Gain (Blue), Phase (Red)")
+        fig.tight_layout()
+        plt.show()
