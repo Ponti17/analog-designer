a8eac909e6882de98068d60c14c200b1e6796754
andreasp <andreas@pedersenmail.com>
2024-05-18 09:43:55 +0200

    addd bode functionality

--- ota.py
+++ ota.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 from utils import Utils
 from transistor import MosDevice
 
@@ -9,6 +11,9 @@ from transistor import MosDevice
 class OTA:
     def __init__(self) -> None:
         self.utils = Utils()
+        
+        # Closed loop gain
+        self.cl_gain: float = 0.0
         
         # Input PMOS
         self.M0 = MosDevice()
@@ -54,10 +59,10 @@ class OTA:
         pmos_ro = self.utils.cascode(self.M3.ro(), self.M4.ro(), self.M4.gm())
         self.rout_val = self.utils.parallel([nmos_ro, pmos_ro])
         
-        self.fp1 = 1 / (2 * np.pi * self.rout_val * self.CL)
-        self.fp2 = self.M1.gm() / (4 * np.pi * self.M1.cgg())
-        self.fp3 = self.M1.gm() / (4 * np.pi * self.M1.cgg())
-        self.fp4 = self.M3.gm() / (4 * np.pi * self.M3.cgg())
+        self.fp1 = (1 / (2 * np.pi * self.rout_val * self.CL)).item()
+        self.fp2 = (self.M1.gm() / (4 * np.pi * self.M1.cgs())).item()
+        self.fp3 = (self.M1.gm() / (4 * np.pi * self.M1.cgs())).item()
+        self.fp4 = (self.M3.gm() / (4 * np.pi * self.M3.cgs())).item()
         
         self.av_val = self.rout_val * self.M0.gm()
         
@@ -80,3 +85,36 @@ class OTA:
         W3 = str("{:.2e}".format(float(self.M3.w_val)))
         W4 = str("{:.2e}".format(float(self.M4.w_val)))
         return {"W0": W0, "W1": W1, "W2": W2, "W3": W3, "W4": W4}
+    
+    def bode(self) -> None:
+        poles = self.poles()
+        p1 = poles[0]
+        p2 = poles[1]
+        p3 = poles[2]
+        p4 = poles[3]
+        den4 = [1, p1+p2+p3+p4, p1*p2+p1*p3+p1*p4+p2*p3+p2*p4+p3*p4, p1*p2*p3+p1*p2*p4+p1*p3*p4+p2*p3*p4, p1*p2*p3*p4]
+        K = den4[-1] * self.av()
+        
+        sys = sp.signal.TransferFunction([K], den4)
+        w, mag, phase = sp.signal.bode(sys)
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 8)
+
+        ax.set_xlabel('Frequency [Hz]')
+        ax.set_ylabel('Magnitude [dB]')
+        ax.semilogx(w, mag)
+
+        ax2 = ax.twinx()
+        ax2.semilogx(w, phase, color="red")
+        ax2.set_ylabel('Phase [deg]')
+        ax.grid()
+
+        for i in range(0, len(mag)):
+            if mag[i] < self.cl_gain:
+                print("Phase Margin: {}".format(180+phase[i]))
+                break
+
+        plt.title("Gain (Blue), Phase (Red)")
+        fig.tight_layout()
+        plt.show()
