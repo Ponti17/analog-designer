2216078cf1631bb6b449112a895d0cd39c62e2d3
andreasp <andreas@pedersenmail.com>
2024-05-21 15:57:50 +0200

    compact init of twostage

--- twostage.py
+++ twostage.py
@@ -11,61 +11,42 @@ from transistor import MosDevice
 class TwoStage:
     def __init__(self) -> None:
         self.utils = Utils()
-        
         # Closed loop gain
         self.cl_gain: float = 0.0
-        
         # Input PMOS
         self.M0 = MosDevice()
-        
         # NMOS Mirror
         self.M1 = MosDevice()
-        
         # NMOS Mirror Cascode
         self.M2 = MosDevice()
-        
         # NMOS 2nd stage
         self.M3 = MosDevice()
-        
         # PMOS 2nd stage load
         self.M4 = MosDevice()
-        
         # Miller capacitor
         self.Cc:float           = 0.0
-        
         # Output stage currnet
         self.iout: float        = 0.0
-        
         # Input pair tail current
         self.itail: float       = 0.0
-        
         # 1st stage gm
         self.gm_1st: float      = 0.0
-        
         # 1st stage output resistance
         self.rout_1st: float    = 0.0
-        
         # 2nd stage gm
         self.gm_2nd: float      = 0.0
-        
         # 2nd stage output resistance
         self.rout_2nd: float    = 0.0
-        
         # 1st stage gain
         self.av_1st: float      = 0.0
-        
         # 2nd stage gain
         self.av_2nd: float      = 0.0
-        
         # 2nd stage load capacitor
         self.CL: float          = 0.0
-        
         # Dominant miller pole
         self.fp1: float     = 0.0
-        
         # Output pole
         self.fp2: float     = 0.0
-        
         # 1st stage mirror pole
         self.fp3: float     = 0.0
         
