1db4a0c1175ad848a92e68d2163e81e324e21c43
andreasp <andreas@pedersenmail.com>
2024-05-21 16:13:51 +0200

    make folded cascode twostage

--- folded_cascode.py
+++ folded_cascode.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 from utils import Utils
 from transistor import MosDevice
@@ -9,6 +11,8 @@ from transistor import MosDevice
 class FoldedCascode:
     def __init__(self) -> None:
         self.utils = Utils()
+        # Closed loop gain
+        self.cl_gain: float = 0.0
         # input NMOS
         self.M0 = MosDevice()
         # input PMOS
@@ -21,18 +25,38 @@ class FoldedCascode:
         self.M4 = MosDevice()
         # PMOS CCS Cascode
         self.M5 = MosDevice()
-        
+        # 2nd stage NMOS
+        self.M6 = MosDevice()
+        # 2nd stage PMOS
+        self.M7 = MosDevice()
+        # Miller capacitor
+        self.Cc:float           = 0.0
+        # Output stage currnet
+        self.iout: float        = 0.0
+        # Input pair tail current
         self.itail: float       = 0.0
-        self.GM_val: float      = 0.0
-        self.rout_val: float    = 0.0
+        # 1st stage gm
+        self.gm_1st: float      = 0.0
+        # 1st stage output resistance
+        self.rout_1st: float    = 0.0
+        # 2nd stage gm
+        self.gm_2nd: float      = 0.0
+        # 2nd stage output resistance
+        self.rout_2nd: float    = 0.0
+        # 1st stage gain
+        self.av_1st: float      = 0.0
+        # 2nd stage gain
+        self.av_2nd: float      = 0.0
+        # 2nd stage load capacitor
         self.CL: float          = 0.0
-        
-        # Output Pole
+        # Dominant miller pole
         self.fp1: float     = 0.0
-        # NMOS Mirror Pole
+        # Output pole
         self.fp2: float     = 0.0
-        # NMOS Mirror Zero
-        self.fz1: float     = 0.0
+        # 1st stage mirror pole
+        self.fp3: float     = 0.0
+
+        self.twostage: bool = False
         
     def init(self) -> None:
         self.M0.set_id(self.itail/2)
@@ -41,30 +65,48 @@ class FoldedCascode:
         self.M3.set_id(self.itail/2)
         self.M4.set_id(self.itail)
         self.M5.set_id(self.itail/2)
+        if self.twostage:
+            self.M6.set_id(self.iout)
+            self.M7.set_id(self.iout)
 
         self.__calculate()
             
     def __calculate(self) -> None:
         # Assume worst case GM
-        self.GM_val = min(self.M0.gm(), self.M1.gm())
-        nmos_ro = self.utils.cascode(self.M2.ro(), self.M3.ro(), self.M3.gm())
-        pmos_ro = self.utils.cascode(self.M4.ro(), self.M5.ro(), self.M5.gm())
-        self.rout_val = self.utils.parallel([nmos_ro, pmos_ro])
-        
-        self.fp1 = 1 / (2 * np.pi * self.rout_val * self.CL)
-        self.fp2 = self.M2.gm() / (2 * np.pi * 2 * self.M2.cgs())
+        self.gm_1st = min(self.M0.gm(), self.M1.gm())
+        pmos_ro_1st = self.utils.cascode(self.M4.ro(), self.M5.ro(), self.M5.gm())
+        nmos_ro_1st = self.utils.cascode(self.M2.ro(), self.M3.ro(), self.M3.gm())
+        self.rout_1st = self.utils.parallel([pmos_ro_1st, nmos_ro_1st])
+        self.av_1st = self.rout_1st * self.gm_1st
+
+        if self.twostage:
+            self.gm_2nd   = self.M6.gm()
+            self.rout_2nd = self.utils.parallel([self.M6.ro(), self.M7.ro()])
+            self.av_2nd   = self.rout_2nd * self.gm_2nd
+            self.fp1 = (1 / (2 * np.pi * self.Cc * (1 + self.av_2nd) * self.rout_1st))
+            self.fp2 = (self.M6.gm() / (2 * np.pi * self.CL))
+            self.fp3 = (self.M2.gm() / (2 * np.pi * self.M2.cgs()))
+        else:
+            self.fp1 = 1 / (2 * np.pi * self.rout_1st * self.CL)
+            self.fp2 = self.M2.gm() / (2 * np.pi * self.M2.cgs())
         
     def av(self) -> float:
-        return self.rout_val * self.GM_val
+        if self.twostage:
+            return self.av_1st * self.av_2nd
+        else:
+            return self.av_1st
     
     def rout(self) -> float:
-        return self.rout_val
+        if self.twostage:
+            return self.rout_2nd
+        else:
+            return self.rout_1st
     
     def poles(self) -> list[float]:
-        return [self.fp1, self.fp2]
-
-    def GM(self) -> float:
-        return self.GM_val
+        if self.twostage:
+            return [self.fp1, self.fp2, self.fp3]
+        else:
+            return [self.fp1, self.fp2]
     
     def size(self) -> dict[str, str]:
         W0 = str("{:.2e}".format(np.abs(float(self.M0.w_val))))
@@ -73,10 +115,15 @@ class FoldedCascode:
         W3 = str("{:.2e}".format(np.abs(float(self.M3.w_val))))
         W4 = str("{:.2e}".format(np.abs(float(self.M4.w_val))))
         W5 = str("{:.2e}".format(np.abs(float(self.M5.w_val))))
-        return {"W0": W0, "W1": W1, "W2": W2, "W3": W3, "W4": W4, "W5": W5}
+        if self.twostage:
+            W6 = str("{:.2e}".format(np.abs(float(self.M6.w_val))))
+            W7 = str("{:.2e}".format(np.abs(float(self.M7.w_val))))
+            return {"W0": W0, "W1": W1, "W2": W2, "W3": W3, "W4": W4, "W5": W5, "W6": W6, "W7": W7}
+        else:
+            return {"W0": W0, "W1": W1, "W2": W2, "W3": W3, "W4": W4, "W5": W5}
     
     def characterize(self, latex: bool) -> None:
-        av  = np.round(20*np.log10(self.av()), 2)
+        av   = np.round(20*np.log10(self.av()), 2)
         poles = self.poles()
         sizes = self.size()
         power = (self.itail * 4) * 1.2 # Assuming 1.2 VDD
@@ -84,13 +131,25 @@ class FoldedCascode:
         poles_formatted = []
         for pole in poles:
             poles_formatted.append(str("{:.2e}".format(pole)))
-            
-        table = [["1st Stage Gain"  , av    , "Output Pole" , poles[0]  , "M0", sizes["W0"], self.M0.gateL],
-                 [None              , None  , "Mirror Pole" , poles[1]  , "M1", sizes["W1"], self.M1.gateL],
-                 [None              , None  , None          , None      , "M2", sizes["W2"], self.M2.gateL],
-                 [None              , None  , None          , None      , "M3", sizes["W3"], self.M3.gateL],
-                 [None              , None  , None          , None      , "M4", sizes["W4"], self.M4.gateL],
-                 [None              , None  , None          , None      , "M4", sizes["W5"], self.M5.gateL]]
+        
+        if self.twostage:
+            av1  = np.round(20*np.log10(self.av_1st), 2)
+            av2  = np.round(20*np.log10(self.av_2nd), 2)
+            table = [["1st Stage Gain"  , av1   , "Dominant Pole"   , poles[0]  , "M0", sizes["W0"], self.M0.gateL],
+                    ["2nd Stage Gain"   , av2   , "Output Pole"     , poles[1]  , "M1", sizes["W1"], self.M1.gateL],
+                    ["Total Gain"       , av    , "Mirror Pole"     , poles[2]  , "M2", sizes["W2"], self.M2.gateL],
+                    [None              , None   , None              , None      , "M3", sizes["W3"], self.M3.gateL],
+                    [None              , None   , None              , None      , "M4", sizes["W4"], self.M4.gateL],
+                    [None              , None   , None              , None      , "M5", sizes["W5"], self.M5.gateL],
+                    [None              , None   , None              , None      , "M6", sizes["W6"], self.M5.gateL],
+                    [None              , None   , None              , None      , "M7", sizes["W7"], self.M5.gateL]]
+        else:
+            table = [["1st Stage Gain" , av    , "Output Pole" , poles[0]  , "M0", sizes["W0"], self.M0.gateL],
+                    [None              , None  , "Mirror Pole" , poles[1]  , "M1", sizes["W1"], self.M1.gateL],
+                    [None              , None  , None          , None      , "M2", sizes["W2"], self.M2.gateL],
+                    [None              , None  , None          , None      , "M3", sizes["W3"], self.M3.gateL],
+                    [None              , None  , None          , None      , "M4", sizes["W4"], self.M4.gateL],
+                    [None              , None  , None          , None      , "M5", sizes["W5"], self.M5.gateL]]
         
         if latex:
             format = "latex"
@@ -98,3 +157,38 @@ class FoldedCascode:
             format = "fancy_outline"
         print(tabulate(table, headers=["Gain", "dB", "Pole", "Hz", "Device", "Width", "Length"], tablefmt=format, floatfmt=".2e"))
         print("Power Consumption: {:.2e} W".format(power))
+        
+    def bode(self) -> None:
+        if not self.twostage:
+            return
+        poles = self.poles()
+        p1 = poles[0]
+        p2 = poles[1]
+        p3 = poles[2]
+        den3 = [1, p1+p2+p3, p1*p2+p3*p1+p2*p3, p1*p2*p3]
+        K = den3[-1] * self.av()
+        
+        w = np.logspace(1, 9, 1000)
+        sys = sp.signal.TransferFunction([K], den3)
+        w, mag, phase = sp.signal.bode(sys, w)
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(12, 8)
+
+        ax.set_xlabel('Frequency [Hz]')
+        ax.set_ylabel('Magnitude [dB]')
+        ax.semilogx(w, mag)
+
+        ax2 = ax.twinx()
+        ax2.semilogx(w, phase, color="red")
+        ax2.set_ylabel('Phase [deg]')
+        ax.grid()
+
+        for i in range(0, len(mag)):
+            if mag[i] < self.cl_gain:
+                print("Phase Margin: {}".format(180+phase[i]))
+                break
+
+        plt.title("Gain (Blue), Phase (Red)")
+        fig.tight_layout()
+        plt.show()
--- main_foldedcascode.py
+++ main_foldedcascode.py
@@ -2,15 +2,28 @@ import numpy as np
 from folded_cascode import FoldedCascode
 
 if __name__ == "__main__":
+    # M0: Input NMOS
+    # M1: Input PMOS
+    # M2: NMOS Mirror
+    # M3: NMOS Mirror Cascode
+    # M4: PMOS CCS
+    # M5: PMOS CCS Cascode
+    # M6: 2nd stage NMOS
+    # M7: 2nd stage PMOS
     fd = FoldedCascode()
     fd.itail = 570e-9
+    fd.iout  = 1e-6
     fd.CL = 1e-12
+    fd.Cc = 100e-15
+    fd.twostage = True
     fd.M0.model = "nch_25"
     fd.M1.model = "pch_25"
     fd.M2.model = "nch"
     fd.M3.model = "nch"
     fd.M4.model = "pch"
     fd.M5.model = "pch"
+    fd.M6.model = "nch_25"
+    fd.M7.model = "pch_25"
     
     fd.M0.gateL = 1e-6
     fd.M1.gateL = 1e-6
@@ -18,6 +31,8 @@ if __name__ == "__main__":
     fd.M3.gateL = 1e-6
     fd.M4.gateL = 1e-6
     fd.M5.gateL = 1e-6
+    fd.M6.gateL = 1e-6
+    fd.M7.gateL = 1e-6
     
     fd.M0.vdsrc = 0.3
     fd.M1.vdsrc = 0.3
@@ -25,6 +40,8 @@ if __name__ == "__main__":
     fd.M3.vdsrc = 0.3
     fd.M4.vdsrc = 0.3
     fd.M5.vdsrc = 0.3
+    fd.M6.vdsrc = 0.6
+    fd.M7.vdsrc = 0.6
     
     fd.M0.gmoverid = 27
     fd.M1.gmoverid = 27
@@ -32,6 +49,9 @@ if __name__ == "__main__":
     fd.M3.gmoverid = 21
     fd.M4.gmoverid = 20
     fd.M5.gmoverid = 17
+    fd.M6.gmoverid = 17
+    fd.M7.gmoverid = 17
     
     fd.init()
     fd.characterize(latex = False)
+    fd.bode()
