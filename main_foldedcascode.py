fc416936dc356eb9167f7552e93ebb22f8ca110d
andreasp <andreas@pedersenmail.com>
2024-05-23 12:05:00 +0200

    initial folded cascode design

--- main_foldedcascode.py
+++ main_foldedcascode.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     # M6: 2nd stage NMOS
     # M7: 2nd stage PMOS
     fd = FoldedCascode()
-    fd.itail = 570e-9
-    fd.iout  = 1e-6
+    fd.itail = 470e-9
+    fd.iout  = 3.7e-6
     fd.CL = 1e-12
-    fd.Cc = 100e-15
+    fd.Cc = 1e-12
     fd.twostage = True
     fd.M0.model = "nch_25"
     fd.M1.model = "pch_25"
@@ -24,34 +24,46 @@ if __name__ == "__main__":
     fd.M5.model = "pch"
     fd.M6.model = "nch_25"
     fd.M7.model = "pch_25"
-    
+    # ---
     fd.M0.gateL = 1e-6
     fd.M1.gateL = 1e-6
-    fd.M2.gateL = 2e-6
+    
+    fd.M2.gateL = 1e-6
     fd.M3.gateL = 1e-6
+    
     fd.M4.gateL = 1e-6
     fd.M5.gateL = 1e-6
+    
     fd.M6.gateL = 1e-6
     fd.M7.gateL = 1e-6
+    # ---
+    fd.M0.vdsrc = 0.5
+    fd.M1.vdsrc = 0.5
     
-    fd.M0.vdsrc = 0.3
-    fd.M1.vdsrc = 0.3
     fd.M2.vdsrc = 0.3
     fd.M3.vdsrc = 0.3
+    
     fd.M4.vdsrc = 0.3
     fd.M5.vdsrc = 0.3
+    
     fd.M6.vdsrc = 0.6
     fd.M7.vdsrc = 0.6
-    
+    # ---
     fd.M0.gmoverid = 27
     fd.M1.gmoverid = 27
-    fd.M2.gmoverid = 21
-    fd.M3.gmoverid = 21
-    fd.M4.gmoverid = 20
+    
+    fd.M2.gmoverid = 17
+    fd.M3.gmoverid = 17
+    
+    fd.M4.gmoverid = 17
     fd.M5.gmoverid = 17
+    
     fd.M6.gmoverid = 17
-    fd.M7.gmoverid = 17
+    fd.M7.gmoverid = 20
     
     fd.init()
+    print(fd.M3.gm())
+    print(fd.M3.cgs())
+    print(fd.M1.cgs())
     fd.characterize(latex = False)
-    fd.bode()
+    # fd.bode()
